de7ecb69ed51efeae5749681a38e3bc04efee2ae
Mulong <45600841+759401524@users.noreply.github.com>
2021-04-28 15:58:46 +0800

    update magic wand lab
    change Serial1 output flag

--- examples/magic_wand_lab/magic_wand_lab.ino
+++ examples/magic_wand_lab/magic_wand_lab.ino
@@ -1,14 +1,14 @@
 /* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-  ==============================================================================*/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==============================================================================*/
 
 #include <Pico4ML.h>
 
@@ -26,209 +26,272 @@
 #include "st7735.h"
 #endif
 
-namespace {
+namespace
+{
+  bool  linked    = false;
+  bool  first    = true;
+  uint8_t send_index  = 0;
 
-// Constants for image rasterization
-constexpr int raster_width = 32;
-constexpr int raster_height = 32;
-constexpr int raster_channels = 3;
-constexpr int raster_byte_count = raster_height * raster_width * raster_channels;
-int8_t raster_buffer[raster_byte_count];
+  uint8_t command[32]  = { 0 };
+  bool  start_flag  = false;
+  int  receive_index  = 0;
+  uint8_t previous_ch  = 0;
 
-// Create an area of memory to use for input, output, and intermediate arrays.
-// The size of this will depend on the model you're using, and may need to be
-// determined by experimentation.
-constexpr int kTensorArenaSize = 30 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
+  /* Constants for image rasterization */
+  constexpr int  raster_width    = 32;
+  constexpr int  raster_height    = 32;
+  constexpr int  raster_channels    = 3;
+  constexpr int  raster_byte_count  = raster_height * raster_width * raster_channels;
+  int8_t    raster_buffer[raster_byte_count];
 
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
+  /*
+   * Create an area of memory to use for input, output, and intermediate arrays.
+   * The size of this will depend on the model you're using, and may need to be
+   * determined by experimentation.
+   */
+  constexpr int  kTensorArenaSize = 30 * 1024;
+  uint8_t    tensor_arena[kTensorArenaSize];
 
-// -------------------------------------------------------------------------------- //
-// UPDATE THESE VARIABLES TO MATCH THE NUMBER AND LIST OF GESTURES IN YOUR DATASET  //
-// -------------------------------------------------------------------------------- //
-constexpr int label_count = 10;
-const char* labels[label_count] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+  tflite::ErrorReporter    *error_reporter = nullptr;
+  const tflite::Model    *model    = nullptr;
+  tflite::MicroInterpreter  *interpreter  = nullptr;
 
-}  // namespace
-bool linked = false;
-uint16_t send_index = 0;
-uint8_t output[328] = {0};
+  /*
+   * -------------------------------------------------------------------------------- //
+   * UPDATE THESE VARIABLES TO MATCH THE NUMBER AND LIST OF GESTURES IN YOUR DATASET  //
+   * -------------------------------------------------------------------------------- //
+   */
+  constexpr int  label_count    = 10;
+  const char  *labels[label_count]  = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+} /* namespace */
 
-void setup() {
-  pinMode(22, INPUT);
-  pinMode(25, OUTPUT);
-  digitalWrite(25, HIGH);
+void setup()
+{
+  pinMode( 25, OUTPUT );
+  digitalWrite( 25, !digitalRead( 25 ) );
 
-  // Start serial
-  Serial.begin(115200);
-  Serial1.begin(115200);
+  /* Start serial */
+  Serial.begin( 115200 );
+  Serial1.begin( 115200 );
 
 #if SCREEN
   ST7735_Init();
-  ST7735_DrawImage(0, 0, 80, 160, arducam_logo);
+  ST7735_DrawImage( 0, 0, 80, 160, arducam_logo );
 #endif
 
-  // Start IMU
+  /* Start IMU */
   SetupIMU();
-  Serial.println("Started");
-  // Set up logging. Google style is to avoid globals or statics because of
-  // lifetime uncertainty, but since this has a trivial destructor it's okay.
-  static tflite::MicroErrorReporter micro_error_reporter;  // NOLINT
+  Serial.println( "Started" );
+  /*
+   * Set up logging. Google style is to avoid globals or statics because of
+   * lifetime uncertainty, but since this has a trivial destructor it's okay.
+   */
+  static tflite::MicroErrorReporter micro_error_reporter; /* NOLINT */
   error_reporter = &micro_error_reporter;
 
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  model = tflite::GetModel(g_magic_wand_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Model provided is schema version %d not equal "
-                         "to supported version %d.",
-                         model->version(), TFLITE_SCHEMA_VERSION);
+  /*
+   * Map the model into a usable data structure. This doesn't involve any
+   * copying or parsing, it's a very lightweight operation.
+   */
+  model = tflite::GetModel( g_magic_wand_model_data );
+  if ( model->version() != TFLITE_SCHEMA_VERSION )
+  {
+    TF_LITE_REPORT_ERROR( error_reporter,
+              "Model provided is schema version %d not equal "
+              "to supported version %d.",
+              model->version(), TFLITE_SCHEMA_VERSION );
     return;
   }
 
-  // Pull in only the operation implementations we need.
-  // This relies on a complete list of all the ops needed by this graph.
-  // An easier approach is to just use the AllOpsResolver, but this will
-  // incur some penalty in code space for op implementations that are not
-  // needed by this graph.
-  static tflite::MicroMutableOpResolver<4> micro_op_resolver;  // NOLINT
+  /*
+   * Pull in only the operation implementations we need.
+   * This relies on a complete list of all the ops needed by this graph.
+   * An easier approach is to just use the AllOpsResolver, but this will
+   * incur some penalty in code space for op implementations that are not
+   * needed by this graph.
+   */
+  static tflite::MicroMutableOpResolver<4> micro_op_resolver; /* NOLINT */
   micro_op_resolver.AddConv2D();
   micro_op_resolver.AddMean();
   micro_op_resolver.AddFullyConnected();
   micro_op_resolver.AddSoftmax();
 
-  // Build an interpreter to run the model with.
+  /* Build an interpreter to run the model with. */
   static tflite::MicroInterpreter static_interpreter(
-    model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+    model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter );
   interpreter = &static_interpreter;
 
-  // Allocate memory from the tensor_arena for the model's tensors.
+  /* Allocate memory from the tensor_arena for the model's tensors. */
   interpreter->AllocateTensors();
 
-  // Set model input settings
-  TfLiteTensor* model_input = interpreter->input(0);
-  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-      (model_input->dims->data[1] != raster_height) ||
-      (model_input->dims->data[2] != raster_width) ||
-      (model_input->dims->data[3] != raster_channels) ||
-      (model_input->type != kTfLiteInt8)) {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Bad input tensor parameters in model");
+  /* Set model input settings */
+  TfLiteTensor *model_input = interpreter->input( 0 );
+  if ( (model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
+       (model_input->dims->data[1] != raster_height) ||
+       (model_input->dims->data[2] != raster_width) ||
+       (model_input->dims->data[3] != raster_channels) ||
+       (model_input->type != kTfLiteInt8) )
+  {
+    TF_LITE_REPORT_ERROR( error_reporter,
+              "Bad input tensor parameters in model" );
     return;
   }
 
-  // Set model output settings
-  TfLiteTensor* model_output = interpreter->output(0);
-  if ((model_output->dims->size != 2) || (model_output->dims->data[0] != 1) ||
-      (model_output->dims->data[1] != label_count) ||
-      (model_output->type != kTfLiteInt8)) {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Bad output tensor parameters in model");
+  /* Set model output settings */
+  TfLiteTensor *model_output = interpreter->output( 0 );
+  if ( (model_output->dims->size != 2) || (model_output->dims->data[0] != 1) ||
+       (model_output->dims->data[1] != label_count) ||
+       (model_output->type != kTfLiteInt8) )
+  {
+    TF_LITE_REPORT_ERROR( error_reporter,
+              "Bad output tensor parameters in model" );
     return;
   }
 #if SCREEN
-  ST7735_FillScreen(ST7735_GREEN);
+  ST7735_FillScreen( ST7735_GREEN );
 
-  ST7735_WriteString(5, 20, "Magic", Font_11x18, ST7735_BLACK, ST7735_GREEN);
-  ST7735_WriteString(30, 45, "Wand", Font_11x18, ST7735_BLACK, ST7735_GREEN);
+  ST7735_WriteString( 5, 20, "Magic", Font_11x18, ST7735_BLACK, ST7735_GREEN );
+  ST7735_WriteString( 30, 45, "Wand", Font_11x18, ST7735_BLACK, ST7735_GREEN );
 #endif
+
+  digitalWrite( 25, !digitalRead( 25 ) );
 }
 
-void loop() {
-  digitalWrite(25, LOW);
-  int accelerometer_samples_read;
-  int gyroscope_samples_read;
-  ReadAccelerometerAndGyroscope(&accelerometer_samples_read, &gyroscope_samples_read);
 
-  // Parse and process IMU data
+void loop()
+{
+  digitalWrite( 25, !digitalRead( 25 ) );
+  int  accelerometer_samples_read;
+  int  gyroscope_samples_read;
+  ReadAccelerometerAndGyroscope( &accelerometer_samples_read, &gyroscope_samples_read );
+
+  /* Parse and process IMU data */
   bool done_just_triggered = false;
-  if (gyroscope_samples_read > 0) {
-    EstimateGyroscopeDrift(current_gyroscope_drift);
-    UpdateOrientation(gyroscope_samples_read, current_gravity, current_gyroscope_drift);
-    UpdateStroke(gyroscope_samples_read, &done_just_triggered);
-    if (digitalRead(22)) {
-      if (!linked) {
-        sleep_ms(5000);
+  if ( gyroscope_samples_read > 0 )
+  {
+    EstimateGyroscopeDrift( current_gyroscope_drift );
+    UpdateOrientation( gyroscope_samples_read, current_gravity, current_gyroscope_drift );
+    UpdateStroke( gyroscope_samples_read, &done_just_triggered );
+    if ( linked )
+    {
+      if ( first )
+      {
+        sleep_ms( 1000 );
+        first = false;
       }
-      linked = true;
-      if (send_index == 0) {
-        memcpy(output, stroke_struct_buffer, 328);
-        // Serial1.write(header, 2);
-        Serial1.write(output , 8);
-        send_index += 8;
-      } else {
-        Serial1.write(output + send_index, 20);
-        send_index += 20;
+      if ( send_index++ % 16 == 0 )
+      {
+        Serial1.write( stroke_struct_buffer, 328 );
       }
-      if (send_index == 328) {
-        send_index = 0;
-      }
-    } else {
-      linked = false;
+    }else  {
+      first    = true;
+      send_index  = 0;
     }
   }
-  if (accelerometer_samples_read > 0) {
-    EstimateGravityDirection(current_gravity);
-    UpdateVelocity(accelerometer_samples_read, current_gravity);
+  if ( accelerometer_samples_read > 0 )
+  {
+    EstimateGravityDirection( current_gravity );
+    UpdateVelocity( accelerometer_samples_read, current_gravity );
   }
-  digitalWrite(25, HIGH);
-  // Wait for a gesture to be done
-  if (done_just_triggered and !digitalRead(22)) {
-    // Rasterize the gesture
-    RasterizeStroke(stroke_points, *stroke_transmit_length, 0.6f, 0.6f, raster_width, raster_height, raster_buffer);
-    for (int y = 0; y < raster_height; ++y) {
+  /* Wait for a gesture to be done */
+  if ( done_just_triggered and !linked )
+  {
+    /* Rasterize the gesture */
+    RasterizeStroke( stroke_points, *stroke_transmit_length, 0.6f, 0.6f, raster_width, raster_height, raster_buffer );
+    for ( int y = 0; y < raster_height; ++y )
+    {
       char line[raster_width + 1];
-      for (int x = 0; x < raster_width; ++x) {
-        const int8_t* pixel = &raster_buffer[(y * raster_width * raster_channels) + (x * raster_channels)];
-        const int8_t red = pixel[0];
-        const int8_t green = pixel[1];
-        const int8_t blue = pixel[2];
-        char output;
-        if ((red > -128) || (green > -128) || (blue > -128)) {
+      for ( int x = 0; x < raster_width; ++x )
+      {
+        const int8_t  *pixel  = &raster_buffer[(y * raster_width * raster_channels) + (x * raster_channels)];
+        const int8_t  red  = pixel[0];
+        const int8_t  green  = pixel[1];
+        const int8_t  blue  = pixel[2];
+        char    output;
+        if ( (red > -128) || (green > -128) || (blue > -128) )
+        {
           output = '#';
-        } else {
+        }else  {
           output = '.';
         }
         line[x] = output;
       }
       line[raster_width] = 0;
-      Serial.println(line);
+      Serial.println( line );
     }
 
-    // Pass to the model and run the interpreter
-    TfLiteTensor* model_input = interpreter->input(0);
-    for (int i = 0; i < raster_byte_count; ++i) {
+    /* Pass to the model and run the interpreter */
+    TfLiteTensor *model_input = interpreter->input( 0 );
+    for ( int i = 0; i < raster_byte_count; ++i )
+    {
       model_input->data.int8[i] = raster_buffer[i];
     }
     TfLiteStatus invoke_status = interpreter->Invoke();
-    if (invoke_status != kTfLiteOk) {
-      TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
+    if ( invoke_status != kTfLiteOk )
+    {
+      TF_LITE_REPORT_ERROR( error_reporter, "Invoke failed" );
       return;
     }
-    TfLiteTensor* output = interpreter->output(0);
+    TfLiteTensor *output = interpreter->output( 0 );
 
-    // Parse the model output
-    int8_t max_score;
-    int max_index;
-    for (int i = 0; i < label_count; ++i) {
+    /* Parse the model output */
+    int8_t  max_score;
+    int  max_index;
+    for ( int i = 0; i < label_count; ++i )
+    {
       const int8_t score = output->data.int8[i];
-      if ((i == 0) || (score > max_score)) {
-        max_score = score;
-        max_index = i;
+      if ( (i == 0) || (score > max_score) )
+      {
+        max_score  = score;
+        max_index  = i;
       }
     }
-    int8_t final_score = ((max_score + 128) * 100) >> 8;
-    TF_LITE_REPORT_ERROR(error_reporter, "Found %s (%d%%)", labels[max_index], final_score);
+    int8_t final_score = ( (max_score + 128) * 100) >> 8;
+    TF_LITE_REPORT_ERROR( error_reporter, "Found %s (%d%%)", labels[max_index], final_score );
 #if SCREEN
     char str[10];
-    sprintf(str, "%d%%", final_score);
+    sprintf( str, "%d%%", final_score );
 
-    ST7735_FillRectangle(0, 80, ST7735_WIDTH, 160 - 80, ST7735_GREEN);
-    ST7735_WriteString(35, 90, labels[max_index], Font_11x18, ST7735_BLACK, ST7735_GREEN);
-    ST7735_WriteString(25, 120, str, Font_11x18, ST7735_BLACK, ST7735_GREEN);
+    ST7735_FillRectangle( 0, 80, ST7735_WIDTH, 160 - 80, ST7735_GREEN );
+    ST7735_WriteString( 35, 90, labels[max_index], Font_11x18, ST7735_BLACK, ST7735_GREEN );
+    ST7735_WriteString( 25, 120, str, Font_11x18, ST7735_BLACK, ST7735_GREEN );
 #endif
   }
 }
+
+
+void serialEvent1()
+{
+  while ( Serial1.available() )
+  {
+    /* get the new byte: */
+    uint8_t current_ch = Serial1.read();
+    Serial.write( current_ch );
+    if ( start_flag )
+    {
+      command[receive_index++] = current_ch;
+    }
+    if ( current_ch == 0xf4 && previous_ch == 0xf5 )
+    {
+      start_flag = true;
+    }else if ( current_ch == 0x0a && previous_ch == 0x0d )
+    {
+      start_flag = false;
+      /* add terminator */
+      command[receive_index - 2] = '\0';
+      /* Serial.write(command,receive_index); */
+
+      receive_index = 0;
+      if ( strcmp( "IND=BLECONNECTED", (const char *) command ) == 0 )
+      {
+        linked = true;
+      }else if ( strcmp( "IND=BLEDISCONNECTED", (const char *) command ) == 0 )
+      {
+        linked = false;
+      }
+    }
+    previous_ch = current_ch;
+  }
+}
+
+
